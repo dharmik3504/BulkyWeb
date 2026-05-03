9d57b6b49d4f4149414eac19f42f519b7719d32e
Dharmik Thakkar <heavynotebook@gmail.com>
2026-05-03 16:18:14 +0530

    added getCategory

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
+using BulkyWeb;
+using BulkyWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyApp.Namespace
 {
     public class CategoryController : Controller
     {
+        private readonly  ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        
         // GET: CategoryController.cs
         public ActionResult Index()
         {
+           List<Category> objCategoryList= _db.Categories.ToList();
             return View();
         }
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("Defaultconnection")));
 
 var app = builder.Build();
 
